18c918d4d118b6acc78951b2385f29b1dafe50b9
kbl <m.pietraszek@gmail.com>
2011-11-20 09:58:06 +0100

    standard moves for pawn

--- lib/chess/chessman/pawn.rb
+++ lib/chess/chessman/pawn.rb
@@ -3,6 +3,17 @@ require 'chess/chessman/base'
 module Chess
   module Chessman
     class Pawn < Base
+      def moves(board)
+        moves = []
+
+        if color == :white
+          moves << Base.position(x, y + 1)
+        else
+          moves << Base.position(x, y - 1)
+        end
+        
+        moves
+      end
     end
   end
 end
--- spec/lib/chess/chessman/pawn_spec.rb
+++ spec/lib/chess/chessman/pawn_spec.rb
@@ -3,12 +3,18 @@ require 'spec_helper'
 module Chess
   module Chessman
     describe Pawn do
+      describe 'Pawn#moves' do
 
-      subject { Pawn.new('a2', :white) }
-
-      it 'should return posible movements' do
-        pending
-        subject.moves.should == ['a3', 'a4']
+        context 'standard movement by one field' do
+          it 'pawn moves one by one field (white)' do
+            p = Pawn.new('c4', :white)
+            p.moves(:board).should == ['c5']
+          end
+          it 'pawn moves one by one field (black)' do
+            p = Pawn.new('c4', :black)
+            p.moves(:board).should == ['c3']
+          end
+        end
       end
 
     end
