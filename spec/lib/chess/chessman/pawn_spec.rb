bf4707407f8474599f8808bf79e0e8b999f3cdd0
kbl <m.pietraszek@gmail.com>
2011-11-21 08:36:43 +0100

    further duplication removal

--- spec/lib/chess/chessman/pawn_spec.rb
+++ spec/lib/chess/chessman/pawn_spec.rb
@@ -7,60 +7,67 @@ module Chess
 
         let(:board) { Board.new }
 
+        def white(field)
+          Pawn.white(field)
+        end
+        def black(field)
+          Pawn.black(field)
+        end
+
         context 'standard movement by one field' do
           it 'pawn moves one by one field (white)' do
-            p = Pawn.white('c4')
+            p = white('c4')
             p.moves(board).should == ['c5']
           end
           it 'pawn moves one by one field (black)' do
-            p = Pawn.black('a4')
+            p = black('a4')
             p.moves(board).should == ['a3']
           end
           it 'shouldnt allow to move from last line (white)' do
-            p = Pawn.white('h8')
+            p = white('h8')
             p.moves(board).should == []
           end
           it 'shouldnt allow to move from last line (black)' do
-            p = Pawn.black('c1')
+            p = black('c1')
             p.moves(board).should == []
           end
         end
 
         context 'movement from first line' do
           it 'should allow move by 2 field from first line (white)' do
-            p = Pawn.white('b2')
+            p = white('b2')
             p.moves(board).should == ['b3', 'b4']
           end
           it 'should allow move by 2 field from first line (black)' do
-            p = Pawn.black('e7')
+            p = black('e7')
             p.moves(board).should == ['e6', 'e5']
           end
         end
 
         context 'movement to occupied position' do
           it 'shouldnt allow for movement to occupied position (white)' do
-            p = Pawn.white('c3')
+            p = white('c3')
 
             b = Board.new
-            b << Pawn.black('c4')
+            b << black('c4')
 
             p.moves(b).should == []
           end
           it 'shouldnt allow for movement to occupied position (black)' do
-            p = Pawn.black('d6')
+            p = black('d6')
 
             b = Board.new
-            b << Pawn.black('d5')
+            b << black('d5')
 
             p.moves(b).should == []
           end
           context 'movement by two fields' do
             it 'shouldnt allow for movement to occupied position (white)' do
               pending
-              p = Pawn.white('E2')
+              p = white('E2')
 
               b = Board.new
-              b << Pawn.black('e3')
+              b << black('e3')
 
               p.moves(b).should == []
             end
