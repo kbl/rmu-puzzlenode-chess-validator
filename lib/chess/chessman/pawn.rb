2fb58b223140d6b903fd209c6d0ae74d8f38d9f6
kbl <m.pietraszek@gmail.com>
2011-11-21 15:47:39 +0100

    common validations moved into proc objects

--- lib/chess/chessman/pawn.rb
+++ lib/chess/chessman/pawn.rb
@@ -15,22 +15,23 @@ module Chess
             other_chessman = board[*move]
             other_chessman && other_chessman.color != chessman.color
           end                                
+
+          first_line_validation = Proc.new do |chessman, board, move|
+            first_line = Base.const_get("FIRST_LINE_#{chessman.color.upcase}")
+            chessman.y == first_line && !board[*move] && @moves[chessman.color][0].valid?(chessman, board)
+          end
           
           @moves[:white] << Move.new(0, 1) do |chessman, board, move|   
             !board[*move]
           end
-          @moves[:white] << Move.new(0, 2) do |chessman, board, move|
-            chessman.y == Base::FIRST_LINE_WHITE && !board[*move] && @moves[:white][0].valid?(chessman, board)
-          end
+          @moves[:white] << Move.new(0, 2, &first_line_validation)
           @moves[:white] << Move.new(-1, 1, &capturing_validation)
           @moves[:white] << Move.new(1, 1, &capturing_validation)
 
           @moves[:black] << Move.new(0, -1) do |chessman, board, move|   
             !board[*move]
           end
-          @moves[:black] << Move.new(0, -2) do |chessman, board, move|
-            chessman.y == Base::FIRST_LINE_BLACK && !board[*move] && @moves[:black][0].valid?(chessman, board)
-          end
+          @moves[:black] << Move.new(0, -2, &first_line_validation)
           @moves[:black] << Move.new(1, -1, &capturing_validation)
           @moves[:black] << Move.new(-1, -1, &capturing_validation)
         end
