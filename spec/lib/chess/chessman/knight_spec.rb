e579cf4176a8ed22f1ad6456976b5cded1931172
kbl <m.pietraszek@gmail.com>
2011-11-22 07:21:59 +0100

    moves for knight in board corner

--- spec/lib/chess/chessman/knight_spec.rb
+++ spec/lib/chess/chessman/knight_spec.rb
@@ -7,9 +7,16 @@ module Chess
 
         let(:board) { Board.new }
 
-        it 'should allow for 8 moves on empty board' do
-          k = Knight.white('d4')
-          k.moves(board).should =~ ['c2', 'e2', 'b3', 'f3', 'b5', 'f5', 'c6', 'e6']
+        context 'empty board' do
+          it 'should allow for 8 moves on empty board' do
+            k = Knight.white('d4')
+            k.moves(board).should =~ ['c2', 'e2', 'b3', 'f3', 'b5', 'f5', 'c6', 'e6']
+          end
+
+          it 'should allow for 2 movements from board corner' do
+            k = Knight.white('h8')
+            k.moves(board).should =~ ['f7', 'g6']
+          end
         end
       end
     end
