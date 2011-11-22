920fa5e863da4b323757e398f66f024fe8c12878
kbl <m.pietraszek@gmail.com>
2011-11-22 07:27:15 +0100

    further specs for knight

--- spec/lib/chess/chessman/knight_spec.rb
+++ spec/lib/chess/chessman/knight_spec.rb
@@ -12,10 +12,23 @@ module Chess
             k = Knight.white('d4')
             k.moves(board).should =~ ['c2', 'e2', 'b3', 'f3', 'b5', 'f5', 'c6', 'e6']
           end
-
           it 'should allow for 2 movements from board corner' do
             k = Knight.white('h8')
             k.moves(board).should =~ ['f7', 'g6']
+          end
+        end
+        context 'capturing' do
+          it 'should allow capturing different color chessman' do
+            k = Knight.white('b8')
+            board << Pawn.black('d7')
+
+            k.moves(board).should =~ ['a6', 'c6', 'd7']
+          end
+          it 'shouldnt allow capturing same color chessman' do
+            k = Knight.white('b8')
+            board << Pawn.white('d7')
+
+            k.moves(board).should =~ ['a6', 'c6']
           end
         end
       end
