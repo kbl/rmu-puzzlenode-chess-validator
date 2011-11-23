5e3a3fc028ea1609db49e1cb9c06ffd39db69355
kbl <m.pietraszek@gmail.com>
2011-11-23 22:12:05 +0100

    pending spec for rook movent

--- spec/lib/chess/chessman/rook_spec.rb
+++ spec/lib/chess/chessman/rook_spec.rb
@@ -12,6 +12,14 @@ module Chess
           r.moves(board).should =~ ['d1', 'd2', 'd3', 'd5', 'd6', 'd7', 'd8', 'a4', 'b4', 'c4', 'e4', 'f4', 'g4', 'h4']
         end
 
+        it 'shouldnt allow for moving beyond other chessman' do
+          pending
+          board << Pawn.white('a2')
+
+          r = Rook.white('a1')
+          r.moves(board).should =~ ['b1', 'c1', 'd1', 'e1', 'f1', 'g1', 'h1']
+        end
+
       end
     end
   end
