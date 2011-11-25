733d766a0046b3bce7a6f0ba32abf714b8cf7773
kbl <m.pietraszek@gmail.com>
2011-11-25 14:01:51 +0100

    more detailed spec for bishop

--- spec/lib/chess/chessman/bishop_spec.rb
+++ spec/lib/chess/chessman/bishop_spec.rb
@@ -7,9 +7,23 @@ module Chess
 
         let(:board) { Board.new }
 
-        it 'should allow for movement on horizontal/vertical lines' do
+        it 'should allow for movement on skew lines' do
           r = Bishop.white('f6')
           r.moves(board).should =~ ['g7', 'h8', 'e5', 'd4', 'c3', 'b2', 'a1', 'e7', 'd8', 'g5', 'h4']
+        end
+
+        it 'should allow for capturing' do
+          r = Bishop.white('f6')
+          board << Bishop.black('b2')
+
+          r.moves(board).should =~ ['g7', 'h8', 'e5', 'd4', 'c3', 'b2', 'e7', 'd8', 'g5', 'h4']
+        end
+
+        it 'shouldnt allow for jumping over chessmans of same color' do
+          r = Bishop.white('f6')
+          board << Bishop.white('g7')
+
+          r.moves(board).should =~ ['e5', 'd4', 'c3', 'b2', 'a1', 'e7', 'd8', 'g5', 'h4']
         end
         
       end
