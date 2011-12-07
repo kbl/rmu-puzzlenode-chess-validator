f5f3b1ec1bfe31cb086db648d6d2b2b60b32b1f0
kbl <m.pietraszek@gmail.com>
2011-12-07 23:47:04 +0100

    output formatter for validator

--- lib/chess/output_formatter.rb
+++ lib/chess/output_formatter.rb
@@ -2,6 +2,7 @@ module Chess
   module OutputFormatter
 
     def self.format_output(stream, output)
+      output.each { |validation_result| stream.puts(validation_result) }
     end
 
   end
--- spec/lib/chess/output_formatter_spec.rb
+++ spec/lib/chess/output_formatter_spec.rb
@@ -4,13 +4,26 @@ module Chess
   describe OutputFormatter do
     describe 'OutputFormatter.format_output' do
       it 'should properly format output message' do
+        output = ''
+        File.open(path('simple_results.txt')) do |file|
+          file.each { |line| output << line }
+        end
+
         validator = RmuValidator.new(path('simple_board.txt'))
         input = path('simple_moves.txt')
         output_stream = StringIO.new
 
         OutputFormatter.format_output(output_stream, validator.validate(input))
 
-        output_stream.string.should == ''
+        output_stream.string.should == output
+      end
+      it 'should pass puzzlenode (:' do
+        validator = RmuValidator.new(path('complex_board.txt'))
+        input = path('complex_moves.txt')
+
+        file = File.new('complex_results.txt', 'w')
+        OutputFormatter.format_output(file, validator.validate(input))
+        file.close
       end
     end
   end
